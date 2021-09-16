26f23988c0764834a0570a60b2ef91e610780bbf
Sungwoo Park <glapark@datamonad.com>
2021-09-15 23:31:06 -0700

    clean DAG, TezClientUtils, JavaOptsChecker

--- tez-api/src/main/java/org/apache/tez/client/TezClientUtils.java
+++ tez-api/src/main/java/org/apache/tez/client/TezClientUtils.java
@@ -19,58 +19,19 @@
 package org.apache.tez.client;
 
 import com.google.common.annotations.VisibleForTesting;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.hadoop.classification.InterfaceAudience.Private;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.yarn.api.ApplicationConstants;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
-import org.apache.tez.common.JavaOptsChecker;
-import org.apache.tez.dag.api.TezConfiguration;
 import org.apache.tez.dag.api.TezConstants;
-import org.apache.tez.dag.api.TezException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
 import java.util.List;
 
 @Private
 public class TezClientUtils {
 
   private static Logger LOG = LoggerFactory.getLogger(TezClientUtils.class);
-
-  @Private
-  public static String addDefaultsToTaskLaunchCmdOpts(String vOpts, Configuration conf,
-      JavaOptsChecker javaOptsChecker) throws TezException {
-    String vConfigOpts = "";
-    String taskDefaultOpts = conf.get(TezConfiguration.TEZ_TASK_LAUNCH_CLUSTER_DEFAULT_CMD_OPTS,
-        TezConfiguration.TEZ_TASK_LAUNCH_CLUSTER_DEFAULT_CMD_OPTS_DEFAULT);
-    if (taskDefaultOpts != null && !taskDefaultOpts.isEmpty()) {
-      vConfigOpts = taskDefaultOpts + " ";
-    }
-    String defaultTaskCmdOpts = TezConfiguration.TEZ_TASK_LAUNCH_CMD_OPTS_DEFAULT;
-    if (vOpts != null && !vOpts.isEmpty()) {
-      // Only use defaults if nothing is specified by the user
-      defaultTaskCmdOpts = "";
-    }
-
-    vConfigOpts = vConfigOpts + conf.get(TezConfiguration.TEZ_TASK_LAUNCH_CMD_OPTS,
-        defaultTaskCmdOpts);
-    if (vConfigOpts != null && !vConfigOpts.isEmpty()) {
-      // Add options specified in the DAG at the end.
-      vOpts = vConfigOpts + " " + vOpts;
-    }
-
-    if (javaOptsChecker != null) {
-      javaOptsChecker.checkOpts(vOpts);
-    }
-
-    return vOpts;
-  }
 
   @Private
   @VisibleForTesting
@@ -83,17 +44,5 @@ public class TezClientUtils {
         + ApplicationConstants.LOG_DIR_EXPANSION_VAR);
     vargs.add("-D" + TezConstants.TEZ_ROOT_LOGGER_NAME + "=" + logLevel
         + "," + TezConstants.TEZ_CONTAINER_LOGGER_NAME);
-  }
-
-  public static byte[] getResourceSha(URI uri, Configuration conf) throws IOException {
-    InputStream is = null;
-    try {
-      is = FileSystem.get(uri, conf).open(new Path(uri));
-      return DigestUtils.sha256(is);
-    } finally {
-      if (is != null) {
-        is.close();
-      }
-    }
   }
 }
