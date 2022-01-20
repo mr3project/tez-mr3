ba5c15725c37db2c36578b7b6f327aaf64cbbc9c
Sungwoo Park <glapark@datamonad.com>
2022-01-20 04:50:50 -0800

    no NPE when PipelinedSorter threads gets interrupted and LogicalOutput.close() is closed

--- tez-runtime-library/src/main/java/org/apache/tez/runtime/library/output/OrderedPartitionedKVOutput.java
+++ tez-runtime-library/src/main/java/org/apache/tez/runtime/library/output/OrderedPartitionedKVOutput.java
@@ -213,6 +213,11 @@ public class OrderedPartitionedKVOutput extends AbstractLogicalOutput {
       boolean isLastEvent = true;
       String auxiliaryService = conf.get(TezConfiguration.TEZ_AM_SHUFFLE_AUXILIARY_SERVICE_ID,
           TezConfiguration.TEZ_AM_SHUFFLE_AUXILIARY_SERVICE_ID_DEFAULT);
+      if (sorter.getFinalIndexFile() == null) {
+        // return an empty list because TezSpillRecord() throws NPE
+        // this occurs when PipelinedSorter threads gets interrupted and LogicalOutput.close() is closed
+        return eventList;
+      }
       ShuffleUtils.generateEventOnSpill(eventList, finalMergeEnabled, isLastEvent,
           getContext(), 0, new TezSpillRecord(sorter.getFinalIndexFile(), localFs),
           getNumPhysicalOutputs(), sendEmptyPartitionDetails, getContext().getUniqueIdentifier(),
