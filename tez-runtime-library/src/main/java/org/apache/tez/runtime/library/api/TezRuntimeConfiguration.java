64440c05ea0948d72655ebd388b823a5f775ad9a
Sungwoo Park <glapark@datamonad.com>
2023-12-15 07:42:00 -0800

    Set the default value of tez.runtime.use.free.memory.fetched.input to false

--- tez-runtime-library/src/main/java/org/apache/tez/runtime/library/api/TezRuntimeConfiguration.java
+++ tez-runtime-library/src/main/java/org/apache/tez/runtime/library/api/TezRuntimeConfiguration.java
@@ -563,7 +563,7 @@ public class TezRuntimeConfiguration {
   @ConfigurationProperty(type = "boolean")
   public static final String TEZ_RUNTIME_USE_FREE_MEMORY_FETCHED_INPUT = TEZ_RUNTIME_PREFIX +
       "use.free.memory.fetched.input";
-  public static final boolean TEZ_RUNTIME_USE_FREE_MEMORY_FETCHED_INPUT_DEFAULT = true;
+  public static final boolean TEZ_RUNTIME_USE_FREE_MEMORY_FETCHED_INPUT_DEFAULT = false;
 
   static {
     tezRuntimeKeys.add(TEZ_RUNTIME_IFILE_READAHEAD);
