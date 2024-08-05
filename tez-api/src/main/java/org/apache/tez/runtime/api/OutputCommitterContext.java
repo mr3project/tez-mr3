b37e2200a9a7ef62e3df5cf2e5c49dbc14f9e74f
Venkatasubrahmanian Narayanan <10137808+VenkatSNarayanan@users.noreply.github.com>
2024-08-05 05:06:25 -0700

    TEZ-4547: Add Tez AM JobID to the JobConf (#339) (Venkatasubrahmanian Narayanan reviewed by Laszlo Bodor)
    Unlike Tez, MR3 extends OutputCommitterContext and OutputContext directly with:
      String getDAGID();

--- tez-api/src/main/java/org/apache/tez/runtime/api/OutputCommitterContext.java
+++ tez-api/src/main/java/org/apache/tez/runtime/api/OutputCommitterContext.java
@@ -78,4 +78,6 @@ public interface OutputCommitterContext {
    */
   public int getVertexIndex();
 
+  public String getDAGID();
+
 }
--- tez-api/src/main/java/org/apache/tez/runtime/api/OutputContext.java
+++ tez-api/src/main/java/org/apache/tez/runtime/api/OutputContext.java
@@ -48,4 +48,5 @@ public interface OutputContext extends TaskContext {
    */
   public OutputStatisticsReporter getStatisticsReporter();
 
+  public String getDAGID();
 }
