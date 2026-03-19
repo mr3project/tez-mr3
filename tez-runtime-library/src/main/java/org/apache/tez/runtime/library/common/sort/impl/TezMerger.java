6f755eebaff5dd121144ebdde79f1b2c971ba9a2
mr3project <59910162+mr3project@users.noreply.github.com>
2026-03-19 12:34:31 +0900

    Use ReaderRead for merger segments

--- tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/sort/impl/TezMerger.java
+++ tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/sort/impl/TezMerger.java
@@ -132,11 +132,11 @@ public class TezMerger {
 
   public static class Segment {
     static final byte[] EMPTY_BYTES = new byte[0];
-    Reader reader = null;
+    IFile.ReaderRead reader = null;
     final KeyValueBuffer key = new KeyValueBuffer(EMPTY_BYTES, 0, 0);
     TezCounter mapOutputsCounter = null;
 
-    public Segment(Reader reader, TezCounter mapOutputsCounter) {
+    public Segment(IFile.ReaderRead reader, TezCounter mapOutputsCounter) {
       this.reader = reader;
       this.mapOutputsCounter = mapOutputsCounter;
     }
@@ -199,7 +199,7 @@ public class TezMerger {
       return reader.getPosition();
     }
 
-    Reader getReader() {
+    IFile.ReaderRead getReader() {
       return reader;
     }
 
