20571de4385a7935ccea2e24ae33fcab8fe0ee8d
mr3project <59910162+mr3project@users.noreply.github.com>
2026-04-30 18:35:40 +0900

    Simplify TezMerger key snapshot creation

--- tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/sort/impl/TezMerger.java
+++ tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/sort/impl/TezMerger.java
@@ -671,7 +671,6 @@ public class TezMerger {
 
     @Override
     public long consumeOrderedGrouped(KeyValuesReaderEdge.KeyGroupConsumer consumer) throws Exception {
-      BytesWritable groupedKey = new BytesWritable();
       BytesWritable groupedKeySnapshot = new BytesWritable();
       BytesWritable groupedValue = new BytesWritable();
       DataInputBuffer groupedValueBuffer = new DataInputBuffer();
@@ -681,12 +680,7 @@ public class TezMerger {
       while (hasNext()) {
         Segment firstSegment = pop();
         KeyValueBuffer currentKey = firstSegment.getKey();
-        if (firstSegment.reader.supportsImmutableRawKeyBuffer()) {
-          groupedKey.setDirect(currentKey.getData(), currentKey.getPosition(), currentKey.getLength());
-        } else {
-          copyToWritable(groupedKey, currentKey);
-        }
-        groupedKeySnapshot.set(groupedKey);
+        copyToWritable(groupedKeySnapshot, currentKey);
         consumer.startKey(groupedKeySnapshot);
 
         List<Segment> groupedSegments = new ArrayList<Segment>();
