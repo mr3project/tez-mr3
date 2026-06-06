e6eef26f4e314c930c8ca05402387668b5531223
mr3project <59910162+mr3project@users.noreply.github.com>
2026-06-06 21:01:33 +0900

    Remove invalid raw buffer close calls

--- tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/writers/UnorderedPartitionedKVWriter.java
+++ tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/writers/UnorderedPartitionedKVWriter.java
@@ -878,8 +878,6 @@ public class UnorderedPartitionedKVWriter extends KeyValuesWriterEdge {
             }
           }
         }
-        key.close();
-        val.close();
       } finally {
         if (compressorExternal != null) {
           outputContext.returnCompressor(codec.getCompressorType(), compressorExternal);
